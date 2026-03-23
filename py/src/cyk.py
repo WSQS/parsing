2d659876abc651adb1a258250b68d64cf318cab4
Sophomore <17792626+WSQS@users.noreply.github.com>
2026-03-23 22:33:05 +0800

    feat: add eliminate_epsilon_non_terminal function

--- py/src/cyk.py
+++ py/src/cyk.py
@@ -23,6 +23,26 @@ def get_epsilon_non_terminal(g: Grammar):
             return result
 
 
+def eliminate_epsilon_non_terminal(g: Grammar, non_terminal: str):
+    new_rule = g.rule[:]
+    for l, rs in g.rule:
+        if non_terminal not in rs:
+            continue
+        rs_queue = [rs]
+        rs_result: list[list[str]] = []
+        while len(rs_queue):
+            cur_rs = rs_queue[0]
+            for i, r in enumerate(cur_rs):
+                if r == non_terminal:
+                    new_rs = cur_rs[:i] + cur_rs[i + 1 :]
+                    if non_terminal in new_rs:
+                        rs_queue.append(new_rs)
+                    elif new_rs not in rs_result and len(new_rs) > 0:
+                        rs_result.append(new_rs)
+        for i_rs in rs_result:
+            new_rule.append((l, i_rs))
+
+
 def eliminate_epsilon_rules(g: Grammar):
     epsilon_non_terminals = get_epsilon_non_terminal(g)
     new_rule: list[tuple[str, list[str]]] = []
