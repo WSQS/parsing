74190cdf08daeedc1acb41838996bfa0381f068a
Sophomore <17792626+WSQS@users.noreply.github.com>
2026-03-24 19:48:33 +0800

    fix: refactor epsilon elimination to use per-nonterminal approach

--- py/src/cyk.py
+++ py/src/cyk.py
@@ -24,40 +24,42 @@ def get_epsilon_non_terminal(g: Grammar):
 
 
 def eliminate_epsilon_non_terminal(g: Grammar, non_terminal: str):
-    new_rule = g.rule[:]
+    new_rule: list[tuple[str, list[str]]] = []
     for l, rs in g.rule:
         if non_terminal not in rs:
+            new_rule.append((l, rs))
             continue
         rs_queue = [rs]
         rs_result: list[list[str]] = []
         while len(rs_queue):
-            cur_rs = rs_queue[0]
+            cur_rs = rs_queue.pop(0)
             for i, r in enumerate(cur_rs):
                 if r == non_terminal:
-                    new_rs = cur_rs[:i] + cur_rs[i + 1 :]
-                    if non_terminal in new_rs:
-                        rs_queue.append(new_rs)
-                    elif new_rs not in rs_result and len(new_rs) > 0:
-                        rs_result.append(new_rs)
+                    new_rs1 = cur_rs[:i] + cur_rs[i + 1 :]
+                    if non_terminal in new_rs1:
+                        rs_queue.append(new_rs1)
+                    elif new_rs1 not in rs_result:
+                        rs_result.append(new_rs1)
+                    new_rs2 = cur_rs[:i] + [non_terminal + "'"] + cur_rs[i + 1 :]
+                    if non_terminal in new_rs2:
+                        rs_queue.append(new_rs2)
+                    elif new_rs2 not in rs_result:
+                        rs_result.append(new_rs2)
         for i_rs in rs_result:
             new_rule.append((l, i_rs))
+    g.rule = new_rule
+    new_rule = []
+    for l, rs in g.rule:
+        if l != non_terminal and len(rs) == 0:
+            continue
+        new_rule.append((l, rs))
+    g.rule = new_rule
 
 
 def eliminate_epsilon_rules(g: Grammar):
     epsilon_non_terminals = get_epsilon_non_terminal(g)
-    new_rule: list[tuple[str, list[str]]] = []
-    while True:
-        update = False
-        for l, rs in g.rule:
-            if l in epsilon_non_terminals:
-                continue
-            for i, r in enumerate(rs):
-                if r in epsilon_non_terminals:
-                    new_rule.append((l, rs[:i] + rs[i + 1 :]))
-                    update = True
-        if not update:
-            break
-    g.rule = new_rule
+    for epsilon_non_terminal in epsilon_non_terminals:
+        eliminate_epsilon_non_terminal(g,epsilon_non_terminal)
 
 
 def to_cnf(g: Grammar):
